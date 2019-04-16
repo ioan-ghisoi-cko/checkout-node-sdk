5ec7d89832c5c2767ac4c36697d92eb2952c81ad
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2019-04-16 23:39:48 +0100

    Update Test

--- src/__tests__/basic.test.ts
+++ src/__tests__/basic.test.ts
@@ -2,19 +2,21 @@ const cko = require('../index')('sk_test_43ed9a7f-4799-461d-b201-a70507878b51');
 
 import { expect } from 'chai';
 
-describe('Basic Suite', async () => {
-    it('Dumb test', async () => {
-        expect(true).to.equal(true);
-        let outcome = await cko.payments.request({
-            source: {
-                type: "card",
-                number: '4242424242424242',
-                expiry_month: '06',
-                expiry_year: '2028',
-            },
-            currency: "USD",
-            amount: 100
-        });
-        expect(outcome.http_code).to.equal(201);
-    })
+describe('Basic Suite', () => {
+    it("should work", done => {
+        (async () => {
+            let outcome = await cko.payments.request({
+                source: {
+                    type: "card",
+                    number: '4242424242424242',
+                    expiry_month: '06',
+                    expiry_year: '2028',
+                },
+                currency: "USD",
+                amount: 100
+            });
+            expect(outcome.http_code).to.equal(201);
+            done();
+        })();
+    });
 });
