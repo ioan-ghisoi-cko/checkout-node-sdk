24a2aa096057e3b9f85051d2cef29ad303374fa1
ioan-ghisoi <ionutghisoi95@gmail.com>
2019-07-01 22:17:19 +0100

    Update Import

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export * from "./config/Constants";
 export * from "./config/Environment";
 export * from "./models/request/Sources";
 export * from "./models/response";
-export * from "./api/Payments";
+export * from "./api/";
