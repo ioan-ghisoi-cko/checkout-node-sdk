f1192377d4fef5710e82228122cb3479e456fb7e
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2020-01-31 11:44:46 +0000

    Print Dirs

--- types copy/api/sources/sources.d.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "../../Checkout";
-export default class Sources {
-  constructor(config: config);
-
-  add: (body: any) => Promise<any>;
-}
--- types copy/api/tokens/tokens.d.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "../../Checkout";
-export default class Tokens {
-  constructor(config: config);
-
-  request: (body: any) => Promise<any>;
-}
